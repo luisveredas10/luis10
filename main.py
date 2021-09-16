a74816bb4789fbb918e511a94eea6d78596407cb
Luis-EnriqueE35 <90847472+luisveredas10@users.noreply.github.com>
2021-09-16 13:34:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# NO MODIFICAR NADA BAJO ESTA LINEA
+def main():
+    # NO MODIFICAR NADA SOBRE ESTA LINEA
+    P1 = float(input("P1: "))
+    P2 = float(input("P2: "))
+    P3 = float(input("P3: "))
+    P4 = float(input("P4: "))
+    P5 = float(input("P5: "))
+    P6 = float(input("P6: "))
+
+    # Codigo comienza aqui
+    masa = P1 + P2 + P3 +P4 +P5 + P6
+    libra = 2.2
+    total = masa / libra
+    print(total)
+    # Codigo acaba aqui
+
+
+# NO MODIFICAR NADA BAJO ESTA LINEA
+if __name__ == '__main__':
+    main()
+# NO MODIFICAR NADA SOBRE ESTA LINEA
